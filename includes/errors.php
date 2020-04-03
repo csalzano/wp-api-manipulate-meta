90f8723c231d780d6bbc68f627c53037ce329247
Corey Salzano <corey.salzano@gmail.com>
2020-04-03 11:30:25 -0400

    Implements translatable strings in our error messages.

--- includes/errors.php
+++ includes/errors.php
@@ -6,9 +6,15 @@ namespace WP_API_Manipulate_Meta
 	{
 		public static function cannot_determine_object_type( $rest_base )
 		{
+			$message = sprintf(
+				'%s `%s` %s',
+				__( 'Could not determine whether ', 'wp-api-manipulate-meta' ),
+				$rest_base,
+				__( ' is a post or a taxonomy. Is the post or taxonomy enabled in the REST API? Does it\'s registration specify a `rest_base`?', 'wp-api-manipulate-meta' )
+			);
 			return new \WP_Error(
 				'rest_cannot_determine_object_type',
-				__( 'Could not determine whether `' . $rest_base . '` is a post or a taxonomy. Is the post or taxonomy enabled in the REST API? Does it\'s registration specify a `rest_base`?' ),
+				$message,
 				array( 'status' => 400 )
 			);
 		}
@@ -17,7 +23,7 @@ namespace WP_API_Manipulate_Meta
 		{
 			return new \WP_Error(
 				'rest_invalid_keys_array',
-				__( 'The body of the request is missing an array of meta keys to delete called `keys`.' ),
+				__( 'The body of the request is missing an array of meta keys to delete called `keys`.', 'wp-api-manipulate-meta' ),
 				array( 'status' => 400 )
 			);
 		}
